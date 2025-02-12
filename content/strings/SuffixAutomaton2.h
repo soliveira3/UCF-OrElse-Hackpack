332ea82bb5b34e6f153cbfce399d804c88d657fc
Sachin S <ssk4988@gmail.com>
2025-02-12 16:31:40 -0500

    improve suffix automaton 2 with bucket topsort (#57)

--- content/strings/SuffixAutomaton2.h
+++ content/strings/SuffixAutomaton2.h
@@ -2,9 +2,7 @@
  * Author: Sachin Sivakumar
  * Date: 2025-01-24
  * Description: Adds endpos (number of occurrences of substring),
- * to and from (number of paths to and from node), suffadj (reversed 
- * suffix link graph), topnext and toplink (topsort of nodes
- * by transition and suffix links).
+ * to and from (number of paths to and from node), topsort of nodes
  * Status: works but not stress tested
  */
 #pragma once
@@ -12,31 +10,19 @@
 int endpos; ll to, from = 1; // add to end of st
 a[last].endpos++; // add to end of char loop
 // add to Suffix Automaton
-vi topnext, toplink; vvi suffadj;
+vi topsort;
 // add to end of constructor
-suffadj.resize(sz(a));
-rep(u, 1, sz(a)) suffadj[a[u].link].push_back(u);
-vi seen(sz(a));
-auto dfs = [&](int u, auto &&dfs) {
-	if(seen[u]) return;
-	seen[u] = true;
+vvi buckets(sz(a));
+rep(u, 0, sz(a)) buckets[a[u].len].push_back(u);
+rep(i, 0, sz(buckets))
+	for(int u : buckets[i]) topsort.push_back(u);
+for(int i = sz(a)-1; i >= 0; i--) {
+	int u = topsort[i];
 	for(auto [c, v] : a[u].next)
-		dfs(v, dfs), a[u].from += a[v].from;
-	topnext.push_back(u);
-};
-dfs(0, dfs);
-seen = vi(sz(a));
-auto dfs1 = [&](int u, auto &&dfs1) {
-	if(seen[u]) return;
-	seen[u] = true;
-	for(int v : suffadj[u]) dfs1(v, dfs1);
-	toplink.push_back(u);
-};
-dfs1(0, dfs1);
-rep(i, 0, sz(a)-1) 
-	a[a[toplink[i]].link].endpos += a[toplink[i]].endpos;
-reverse(all(topnext)), reverse(all(toplink));
+		a[u].from += a[v].from;
+	if(u) a[a[u].link].endpos += a[u].endpos;
+}
 a[0].to = 1;
-for(int u : topnext)
+for(int u : topsort)
 	for(auto [c, v] : a[u].next)
 		a[v].to += a[u].to;
