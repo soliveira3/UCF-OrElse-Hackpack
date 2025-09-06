0bbc6885bc1492eb079bea04fbbdbf96caf9c1bc
Samuel Oliveira <sam.l.olive05@gmail.com>
2025-09-05 20:19:34 -0400

    Update Point.h

--- content/geometry/Point.h
+++ content/geometry/Point.h
@@ -4,7 +4,7 @@
  * License: CC0
  * Source: My head with inspiration from tinyKACTL
  * Description: Class to handle points in the plane.
- * 	T can be e.g. double or long long. (Avoid int.)
+ * 	T can be e.g. ld or long long. (Avoid int.)
  * Status: Works fine, used a lot
  */
 #pragma once
@@ -25,13 +25,13 @@ struct Point {
 	T cross(P p) const { return x*p.y - y*p.x; }
 	T cross(P a, P b) const { return (a-*this).cross(b-*this); }
 	T dist2() const { return x*x + y*y; }
-	double dist() const { return sqrt((double)dist2()); }
+	ld dist() const { return sqrt((ld)dist2()); }
 	// angle to x-axis in interval [-pi, pi]
-	double angle() const { return atan2(y, x); }
+	ld angle() const { return atan2(y, x); }
 	P unit() const { return *this/dist(); } // makes dist()=1
 	P perp() const { return P(-y, x); } // rotates +90 degrees
 	P normal() const { return perp().unit(); }
 	// returns point rotated 'a' RADIANS ccw around the origin
-	P rotate(double a) const { return P(x*cos(a)-y*sin(a),x*sin(a)+y*cos(a)); }
+	P rotate(ld a) const { return P(x*cos(a)-y*sin(a),x*sin(a)+y*cos(a)); }
 	friend ostream& operator<<(ostream& os, P p) { return os << "(" << p.x << "," << p.y << ")"; }
 };
