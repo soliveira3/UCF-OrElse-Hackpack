6bc71d6936eafe0398c941cb2e2b8815a54b7a87
Samuel Oliveira <sam.l.olive05@gmail.com>
2025-09-10 15:32:09 -0400

    Update ConvexHull.h - fixed cses convex hull hack

--- content/geometry/ConvexHull.h
+++ content/geometry/ConvexHull.h
@@ -24,7 +24,7 @@ typedef Point<ll> P;
 vector<P> convexHull(vector<P> pts) {
 	if (sz(pts) <= 1) return pts;
 	sort(all(pts));
-	vector<P> h(sz(pts)+1);
+	vector<P> h(2*sz(pts));
 	int s = 0, t = 0;
 	for (int it = 2; it--; s = --t, reverse(all(pts)))
 		for (P p : pts) {
