366192b90a376cbfb40a3c267848557e7678621a
Samuel Oliveira <sam.l.olive05@gmail.com>
2025-09-10 16:36:19 -0400

    Update EulerWalk.h

--- content/graphs/EulerWalk.h
+++ content/graphs/EulerWalk.h
@@ -3,7 +3,7 @@
  * Date: 2019-12-31
  * License: CC0
  * Source: folklore
- * Description: Eulerian undirected/directed path/cycle algorithm.
+ * Description: Eulerian undirected/directed path/cycle algorithm. (visits every edge once)
  * Input should be a vector of (dest, global edge index), where
  * for undirected graphs, forward/backward edges have the same index.
  * Returns a list of nodes in the Eulerian path/cycle with src at both start and end, or
