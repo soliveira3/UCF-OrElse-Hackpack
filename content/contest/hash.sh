6319f67909320d6e532f2ba5710f9263fd290cba
Samuel Oliveira <soliveira@moscow.ptl.ucf.edu>
2025-09-11 15:07:51 -0400

    Adding things back to the hackpack

--- content/contest/hash.sh
+++ content/contest/hash.sh
@@ -1,3 +1,5 @@
 # Hashes a file, ignoring all whitespace and comments. Use for
 # verifying that code was correctly typed.
+# Use chmod +x hash.sh
+# Then ./hash.sh < ./fileName
 cpp -dD -P -fpreprocessed | tr -d '[:space:]'| md5sum |cut -c-6
--- content/number-theory/ModPow.h
+++ content/number-theory/ModPow.h
@@ -7,9 +7,7 @@
  * Status: tested
  */
 #pragma once
-
 const ll mod = 1000000007; // faster if const
-
 ll modpow(ll b, ll e) {
 	ll ans = 1;
 	for (; e; b = b * b % mod, e /= 2)
